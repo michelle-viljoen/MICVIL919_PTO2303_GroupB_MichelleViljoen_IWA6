94bc13e67b767442cf539d00f7a93753205a1792
michelle-viljoen <126607925+michelle-viljoen@users.noreply.github.com>
2023-06-30 13:58:55 +0100

    IWA6 Ch1

---
+++ Challenge1/scripts.js
@@ -0,0 +1,15 @@
+const primaryPhone = 'O748105141'
+const secondaryPhone = '0219131868'
+
+// Only change below this line
+
+const primaryValid = (Number(primaryPhone) ? 'true' : 'false')
+const secondaryValid = (Number(secondaryPhone) ? 'true' : 'false')
+
+
+console.log('Primary phone is valid numerical string:', primaryValid)
+console.log('Secondary phone is valid numerical string:', secondaryValid )
+
+
+
+
